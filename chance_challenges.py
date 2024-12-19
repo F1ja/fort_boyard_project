c846c4a42a44214472820cea570dd2b3f6df567f
Erwanpe <erwan.pelhate@efrei.net>
2024-12-19 16:16:59 +0100

    Add rolling_dice function to implement a dice rolling game
    Introduced a new rolling_dice function with a 3-attempt mechanism where players and a game master roll dice to land a 6 and win. The shell_game function was commented out, and randint was explicitly imported to facilitate dice rolls. This enhances the challenge module with additional gameplay variety.

--- chance_challenges.py
+++ chance_challenges.py
@@ -1,4 +1,5 @@
 import random
+from random import randint
 from operator import truediv
 
 
@@ -22,4 +23,28 @@ def shell_game():
         print("You lost!")
         print("The key was in: " + key)
         return False
-shell_game()
+#shell_game()
+def rolling_dice():
+    attempt=3
+
+    while attempt>0:
+        print("You have " + str(attempt) + " attempts remaining to guess the numbers.")
+        input("Press Enter to roll the dice...")
+        pdice = [randint(1, 6), randint(1, 6)]
+        print("You rolled:", pdice)
+        for f in pdice:
+            if f == 6:
+                print("Congratulations! You win")
+                return True
+        print("Not 6. You are moving to the next attempt")
+        if attempt == 0:
+            print("No one have one, its a draw, no key for you")
+            return False
+        gmdice = [randint(1, 6), randint(1, 6)]
+        print("Game master rolled:", gmdice)
+        for g in gmdice:
+            if g == 6:
+                print("The game master has won the game")
+                return False
+        attempt -= 1
+rolling_dice()
